5f08a9e30014bb92c57573c4cf579323f7cc3b47
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-01 03:25:04 +0600

    modal added

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ inputBtn.addEventListener("click", () => {
 
 // Displaying Data in a List
 const displayPhones = (phone) => {
-  console.log(phone);
+  //   console.log(phone);
   phones.innerHTML = "";
   if (phone.length <= 0) {
     phones.innerHTML = `<div class="text-center text-2xl font-bold">No Results Found</div>`;
@@ -44,6 +44,7 @@ const displayPhones = (phone) => {
         </div>
         <div class="px-6 pt-4 pb-2">
           <button
+          id="detailsBtn" data-micromodal-trigger="modal-1" onclick="loadDetails('${phone[i].slug}')"
             class="bg-blue-500 rounded p-3 font-semibold text-white mr-2 mb-2"
           >
             Details
@@ -58,4 +59,36 @@ const displayPhones = (phone) => {
     }
   }
   toggleLoader();
+};
+
+// Load Details
+const loadDetails = (slug) => {
+  fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.data));
+};
+
+// Displaying Modal
+const displayDetails = (data) => {
+  MicroModal.init({
+    awaitCloseAnimation: true,
+  });
+  document.getElementById("modal-1-title").innerText = data.name;
+  document.getElementById("modal-1-content").innerHTML = `
+  <div class="grid grid-cols-3">
+    <div class="col-span-1">
+      <img src="${data.image}" class="w-1/2" />
+    </div>
+    <div class="col-span-2">
+      <div class="font-bold text-xl mb-2"><span class="font-bold">Name: </span>${data.name}</div>
+      <p class="text-gray-700 text-base"><span class="font-bold">Brand: </span>${data.brand}</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Chip Set: </span>${data.mainFeatures.chipSet}</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Display: </span>${data.mainFeatures.displaySize}</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Memory: </span>${data.mainFeatures.memory}</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Storage: </span>${data.mainFeatures.storage}</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Sensors: </span>${data.mainFeatures.sensors}</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Release Date: </span>${data.releaseDate}</p>
+    </div>
+  </div>
+  `;
 };
