a99125179f0722ad9d571f5580e454e81ae216fe
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-01 04:16:45 +0600

    Data shown in modal with logic

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ inputBtn.addEventListener("click", () => {
 
 // Displaying Data in a List
 const displayPhones = (phone) => {
-  //   console.log(phone);
   phones.innerHTML = "";
   if (phone.length <= 0) {
     phones.innerHTML = `<div class="text-center text-2xl font-bold">No Results Found</div>`;
@@ -75,20 +74,61 @@ const displayDetails = (data) => {
   });
   document.getElementById("modal-1-title").innerText = data.name;
   document.getElementById("modal-1-content").innerHTML = `
-  <div class="grid grid-cols-3">
+  <div class="grid grid-cols-1 md:grid-cols-3">
     <div class="col-span-1">
       <img src="${data.image}" class="w-1/2" />
     </div>
     <div class="col-span-2">
-      <div class="font-bold text-xl mb-2"><span class="font-bold">Name: </span>${data.name}</div>
-      <p class="text-gray-700 text-base"><span class="font-bold">Brand: </span>${data.brand}</p>
-      <p class="text-gray-700 text-base"><span class="font-bold">Chip Set: </span>${data.mainFeatures.chipSet}</p>
-      <p class="text-gray-700 text-base"><span class="font-bold">Display: </span>${data.mainFeatures.displaySize}</p>
-      <p class="text-gray-700 text-base"><span class="font-bold">Memory: </span>${data.mainFeatures.memory}</p>
-      <p class="text-gray-700 text-base"><span class="font-bold">Storage: </span>${data.mainFeatures.storage}</p>
-      <p class="text-gray-700 text-base"><span class="font-bold">Sensors: </span>${data.mainFeatures.sensors}</p>
-      <p class="text-gray-700 text-base"><span class="font-bold">Release Date: </span>${data.releaseDate}</p>
+      <div class="font-bold text-xl mb-2"><span class="font-bold">Name: </span>${
+        data.name
+      }</div>
+      <p class="text-gray-700 text-base"><span class="font-bold">Brand: </span>${
+        data.brand
+      }</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Chip Set: </span>${
+        data.mainFeatures.chipSet
+      }</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Display: </span>${
+        data.mainFeatures.displaySize
+      }</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Memory: </span>${
+        data.mainFeatures.memory
+      }</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Storage: </span>${
+        data.mainFeatures.storage
+      }</p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Sensors: </span><ul>${sensorsHandler(
+        data.mainFeatures.sensors
+      )}</ul></p>
+      <p class="text-gray-700 text-base"><span class="font-bold">Release Date: </span>${
+        data.releaseDate ? data.releaseDate : "No Date Found"
+      }</p>
+      <p class="text-gray-700 text-base">${
+        data.others
+          ? `<span class="font-bold">Other Features: </span> <br>
+          <span class="font-bold">Bluetooth: </span> ${data.others.Bluetooth} <br>
+          <span class="font-bold">GPS: </span> ${data.others.GPS} <br>
+          <span class="font-bold">NFC: </span> ${data.others.NFC} <br>
+          <span class="font-bold">Radio: </span> ${data.others.Radio} <br>
+          <span class="font-bold">USB: </span> ${data.others.USB} <br>
+          <span class="font-bold">WLAN: </span> ${data.others.WLAN} <br>
+          `
+          : ``
+      }</p>
     </div>
   </div>
   `;
+};
+
+// Sensors Handler
+const sensorsHandler = (sensors) => {
+  let sensorsList = "";
+  if (Array.isArray(sensors)) {
+    for (let i = 0; i < sensors.length; i++) {
+      sensorsList += `<li>${i + 1}. ${sensors[i]}</li>`;
+    }
+  } else {
+    sensorsList = `<li>${sensors}</li>`;
+  }
+  return sensorsList;
 };
